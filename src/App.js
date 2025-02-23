c233801e1ebc7e942580986fa53e03004d28806f
Abrar Mustafa <abrarmustafa25@gmail.com>
2025-02-23 11:54:15 +0500

    initializing visit table
    initializing visit table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import VisitTable from './components/VisitTable';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [visits, setVisits] = useState([]);
+
+  const fetchVisits = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/appointment/visit/');
+      setVisits(response.data);
+    } catch (error) {
+      console.error('Error while fetching visits:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchVisits();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Patient Records</h1>
+      <VisitTable visits={visits} />
     </div>
   );
-}
+};
 
 export default App;
