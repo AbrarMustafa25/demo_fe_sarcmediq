fd76bd95d50d89cd03f37842f7bf008f2a63afaa
Abrar Mustafa <abrarmustafa25@gmail.com>
2025-02-23 12:20:36 +0500

    iterating data for visits
    iterating data for visits

--- src/components/VisitTable.js
+++ src/components/VisitTable.js
@@ -3,7 +3,38 @@ import React from 'react';
 const VisitTable = ({visits}) =>{
     return (
         <div>
-            ..
+            <h2>
+                Patient Visits
+            </h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            MR Number
+                        </th>
+                        <th>
+                            Patient Name
+                        </th>
+                        <th>
+                            Date
+                        </th>
+                        <th>
+                            Reason
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {visits}
+                    {visits.map((visit_item) => (
+                        <tr>
+                            <th>
+                                ...
+                            </th>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
+export default VisitTable;
